bff2a97f51f9e446ce5582026c8d549ab91d5633
Gideonweb3 <gideoncharlesweb3@gmail.com>
2024-08-15 17:05:24 +0300

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,26 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",  // Updated to focus on the App directory
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#1D4ED8",
+        secondary: "#9333EA",
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
